021a4635675a4f47ab104f361c35fc2ff19dd88f
Prasad814359 <54134798+Prasad814359@users.noreply.github.com>
2019-08-15 21:06:41 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,24 +1,27 @@
-int dryValue = 1023;
-int wetValue = 0;
-int DryValuep = 0;
+int dryValue = 1023; // maximum raw value given by FC-28 sensor
+int wetValue = 0;    //minimum raw value given by FC-28 sensor
+int DryValuep = 0;  // to map in terms of percentage
 int WetValuep = 100;
-int pump_pin=8;
+int pump_pin=8;    //Digital pin
+
 void setup() {
   Serial.begin(9600);
   pinMode(pump_pin,OUTPUT);
 }
+
+
 void loop() {
-  int rawValue = analogRead(A4);
+  int rawValue = analogRead(A4); //Reading the raw data from FC-28
   Serial.print("Raw: ");
   Serial.print(rawValue);
   Serial.print(" | ");
   
-  int moiture = map(rawValue, dryValue, wetValue, DryValuep, WetValuep);
+  int moiture = map(rawValue, dryValue, wetValue, DryValuep, WetValuep);  //mapping the raw data in terms of percentage
   
   Serial.print("Moiture content: ");
   Serial.print(moiture);
   Serial.println("%");
-  delay(500);
+  delay(500);    
   if(moiture<50)
   {
      Serial.print("pump is turned On");
